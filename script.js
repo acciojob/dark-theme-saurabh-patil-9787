b36fba6aa73b6e831b9750dcab5129d79a8943a8
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-02-28 19:27:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+let container=document.getElementById('app');
+let swapBtn = document.getElementById('swap')
 
+swapBtn.addEventListener('click',swapTheme)
+
+function swapTheme (){
+    container.classList.toggle('day')
+}
